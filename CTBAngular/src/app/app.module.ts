71aec52879f226ae9101918e577a65f1fa341682
ValeDLT98 <vale.delatorre.leon@gmail.com>
2019-10-31 12:43:43 -0500

    post y feed
    post y feed

--- CTBAngular/src/app/app.module.ts
+++ CTBAngular/src/app/app.module.ts
@@ -5,15 +5,25 @@ import{UsersService} from './servicios/users.service';
 import{PostsService} from './servicios/posts.service';
 import{ComentariosService} from './servicios/comentarios.service';
 import{CalificacionsService} from './servicios/calificacions.service';
-
+import {MatDialogModule} from "@angular/material";
 import { AppComponent } from './app.component';
+import { FeedComponent } from './feed/feed.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostComponent } from './post/post.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedComponent,
+    PostComponent,
+    NavbarComponent,
   ],
+  entryComponents:[PostComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [UsersService,PostsService,ComentariosService,CalificacionsService],
   bootstrap: [AppComponent]
---
+++ CTBAngular/src/app/feed/feed.component.ts
@@ -0,0 +1,20 @@
+import { PostComponent } from './../post/post.component';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.css']
+})
+export class FeedComponent  {
+
+  constructor(private dialog: MatDialog) { }
+  
+  openDialog() {
+
+
+    this.dialog.open(PostComponent);
+  }
+
+}
